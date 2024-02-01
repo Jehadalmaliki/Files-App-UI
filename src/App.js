aba91045dc1d08fb965e45a86d05ddc1212990bb
Jehad Almaliki <yourgitemail@example.com>
2024-02-01 07:05:37 +0300

    setup project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1 className="text-3xl font-bold underline text-red-500">
+      Hello world!
+    </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
