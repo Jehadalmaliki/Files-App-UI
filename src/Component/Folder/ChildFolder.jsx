7aebd13f36ec9e69de0e20a6a6cd269218b9c025
Jehad Almaliki <yourgitemail@example.com>
2024-02-03 10:36:04 +0300

    feat: create childe list folder

---
+++ src/Component/Folder/ChildFolder.jsx
@@ -0,0 +1,21 @@
+// Your React component
+
+import React from "react";
+  
+import FolderDetails from "./FolderDetails";
+
+
+
+const ChildFolder = ({currentFolderId}) => {
+  return (
+   
+   
+    
+    
+      <FolderDetails />
+
+
+  );
+};
+
+export default ChildFolder;
