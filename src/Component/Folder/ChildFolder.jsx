bdf89b82d49696ab8852f67f504befc8807f1054
Jehad Almaliki <yourgitemail@example.com>
2024-02-03 13:33:12 +0300

    refactor: remove unrelated file ..

--- src/Component/Folder/ChildFolder.jsx
+++ src/Component/Folder/ChildFolder.jsx
@@ -1,21 +1,11 @@
 // Your React component
 
 import React from "react";
-  
+
 import FolderDetails from "./FolderDetails";
 
-
-
-const ChildFolder = ({currentFolderId}) => {
-  return (
-   
-   
-    
-    
-      <FolderDetails />
-
-
-  );
+const ChildFolder = ({ currentFolderId }) => {
+  return <FolderDetails />;
 };
 
 export default ChildFolder;
