09c30ae5ccdabc7add1d12aafa2d1d710a4ab40a
Jehad Almaliki <yourgitemail@example.com>
2024-02-03 20:04:12 +0300

    feat: add folders route

--- src/App.js
+++ src/App.js
@@ -7,22 +7,26 @@ import FileUpload from "./Component/Files/FileUpload";
 import 'react-toastify/dist/ReactToastify.css';
 import ChildFolder from './Component/Folder/ChildFolder';
 import MyBreadcrumbs from './Component/Breadcrumbs';
+import Folderlist from './Component/Folder/Folderlist';
+
 
 function App() {
   return (
-    <ThemeProvider>
+    <div className="md:container p-2 mx-auto my-1 bg-light-cyan border border-cyan rounded">
       <Router>
         <MyBreadcrumbs />
 
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/folders" element={<Folderlist />} />
+
           <Route path="/folders/:id" element={<ChildFolder />} />
           <Route path="/files" element={<FileList />} />
           <Route path="/upload" element={<FileUpload />} />
           {/* Add other routes as needed */}
         </Routes>
       </Router>
-    </ThemeProvider>
+    </div>
   );
 }
 
