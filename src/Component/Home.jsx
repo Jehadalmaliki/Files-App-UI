cfe4b7e8ed9f082456344eb36f8f00e1167a6925
Jehad Almaliki <yourgitemail@example.com>
2024-02-03 10:35:21 +0300

    feat: remove unrelated files

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,32 +1,28 @@
 // Your React component
 
 import React from "react";
-import FileUpload from "./FileUpload";
-import FolderNavigation from "./FolderNavigation";
-import FileList from "./FileList";
+import FileUpload from "./Files/FileUpload";
+import FolderNavigation from "./Folder/FolderNavigation";
+import FileList from "./Files/FileList";
 import Input from "./ui/Input";
 
-const YourComponent = () => {
+import { Breadcrumbs } from "@material-tailwind/react";
+import Folderlist from "./Folder/Folderlist";
+
+const Home = () => {
   return (
     <div className="md:container p-2 mx-auto my-1 bg-light-cyan border border-cyan rounded">
       <header className="p-4 dark:bg-gray-800 dark:text-gray-100">
         <div className="flex flex-col md:flex-row md:justify-between items-center h-16 mx-auto space-y-2 md:space-y-0 md:space-x-2">
-         
-            <FileUpload />
-            <FolderNavigation />
-     
-          {/* <Input
-            type="search"
-            name="Search"
-            placeholder="Search"
-            inputWidthSize="w-full md:w-1/4" 
-          /> */}
+          {/* <FileUpload /> */}
+          {/* <Breadcrumbs/> */}
+          <FolderNavigation />
         </div>
       </header>
-
-      <FileList />
+      <Folderlist />
+      {/* <FileList /> */}
     </div>
   );
 };
 
-export default YourComponent;
+export default Home;
