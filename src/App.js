63be79ddc9234e23bec9f7b80e1078905c0106f3
Jehad Almaliki <yourgitemail@example.com>
2024-02-03 10:32:59 +0300

    feat: add navigate route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import FileList from "./Component/FileList";
-import FileUpload from "./Component/FileUpload";
-import FolderNavigation from "./Component/FolderNavigation";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from "@material-tailwind/react";
 import Home from "./Component/Home";
-
-
+import FileList from "./Component/Files/FileList";
+import FileUpload from "./Component/Files/FileUpload";
+import 'react-toastify/dist/ReactToastify.css';
+import ChildFolder from './Component/Folder/ChildFolder';
+import MyBreadcrumbs from './Component/Breadcrumbs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
- <Home/>
+    <ThemeProvider>
+      <Router>
+        <MyBreadcrumbs />
 
- 
-      </header>
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/folders/:id" element={<ChildFolder />} />
+          <Route path="/files" element={<FileList />} />
+          <Route path="/upload" element={<FileUpload />} />
+          {/* Add other routes as needed */}
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
